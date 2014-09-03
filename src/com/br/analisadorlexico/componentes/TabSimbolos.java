71ea9152205552256877b074a270d768d64f6f54
Tubalevião <alvaro.prata.filho@gmail.com>
2014-09-03 12:48:15 -0300

    Update TabSimbolos.java

--- src/com/br/analisadorlexico/componentes/TabSimbolos.java
+++ src/com/br/analisadorlexico/componentes/TabSimbolos.java
@@ -3,10 +3,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TabSimbolos {
-	Map<Integer,Token> simbolos = new HashMap<Integer,Token>();
-	Token token = new Token();
+	private Map<Integer,Token> simbolos = new HashMap<Integer,Token>();
+	private Token token = new Token();
 	
-	//Inserindo os tokens v·lidos na Tabela de SÌmbolos
+	//Inserindo os tokens v√°lidos na Tabela de S√≠mbolos
 	
 	
 }
