0bf052b389f62085c0e4ab174b3dbd0e399ecfd5
Tubalevião <alvaro.prata.filho@gmail.com>
2014-09-13 11:17:32 -0300

    Update TabSimbolos.java
    Terminado. Provavelmente.

--- src/com/br/analisadorlexico/componentes/TabSimbolos.java
+++ src/com/br/analisadorlexico/componentes/TabSimbolos.java
@@ -1,19 +1,48 @@
-package com.br.analisadorlexico.componentes;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+
 
 public class TabSimbolos {
-	Map<Integer,Token> simbolos = new HashMap<Integer,Token>();
-	Token token = new Token();
+	private static TabSimbolos instancia = new TabSimbolos();
+	private ArrayList<Token> listaToken = new ArrayList<Token>();
 	
-	//Inserindo os tokens válidos na Tabela de Símbolos
-		
-	
-	//public Token pesquisaPalavra(String palavra);
-	// Pesquisa feita no mapa simbolos. Se encontrar palavra compatível, beleza. Senão, retorna erro.
-	
-	public Token pesquisaPalavra(String palavra){
-		// caso não ache token, retorna null
-		return token;
+	private TabSimbolos(){
+		listaToken.add(new Token(1,"NUM_INT"));
+		listaToken.add(new Token(2,"NUM_FLOAT"));
+		listaToken.add(new Token(3,"LITERAL"));
+		listaToken.add(new Token(4,"ID"));
+		listaToken.add(new Token(5,"REL_OP"));
+		listaToken.add(new Token(6,"ADDSUB_OP"));
+		listaToken.add(new Token(7,"MULTDIV_OP"));
+		listaToken.add(new Token(8,"ATTRIB_OP"));
+		listaToken.add(new Token(9,"TERM"));
+		listaToken.add(new Token(10,"L_PAR"));
+		listaToken.add(new Token(11,"R_PAR"));
+		listaToken.add(new Token(12,"LOGIC_VAL"));
+		listaToken.add(new Token(13,"LOGIC_OP"));
+		listaToken.add(new Token(14,"TYPE"));
+		listaToken.add(new Token(15,"PROGRAM"));
+		listaToken.add(new Token(16,"END_PROG"));
+		listaToken.add(new Token(17,"BEGIN"));
+		listaToken.add(new Token(18,"END"));
+		listaToken.add(new Token(19,"IF"));
+		listaToken.add(new Token(20,"THEN"));
+		listaToken.add(new Token(21,"ELSE"));
+		listaToken.add(new Token(22,"FOR"));
+		listaToken.add(new Token(23,"WHILE"));
+		listaToken.add(new Token(24,"DECLARE"));
+		listaToken.add(new Token(25,"TO"));
+	}
+	public int pesquisaPalavra(String token){
+		int result=0;
+		for (Token ltoken : listaToken){
+			if(ltoken.getToken().equals(token))
+				result = ltoken.getId();
+			else
+				result = 0;
+		}
+		return result;
+	}
+	public TabSimbolos getInstance(){
+		return instancia;
 	}
 }
