0cd8f4a03bf0bc8eb827b65a9d7d4496e02df808
Tubalevião <alvaro.prata.filho@gmail.com>
2014-09-13 16:31:20 -0300

    Update Codigo.java
    SPAAAAAAARTAAAAAAAAAAAAAAAA!!!

--- src/com/br/analisadorlexico/componentes/Codigo.java
+++ src/com/br/analisadorlexico/componentes/Codigo.java
@@ -2,11 +2,13 @@ public class Codigo {
 	private int linha;
 	private int coluna;
 	private String token;
+	private String lexema;
 	
-	public Codigo(int linha, int coluna, String token){
+	public Codigo(int linha, int coluna, String token, String lexema){
 		this.linha = linha;
 		this.coluna = coluna;
 		this.token = token;
+		this.lexema = lexema;
 	}
 	public int getLinha(){
 		return this.linha;
@@ -16,5 +18,8 @@ public class Codigo {
 	}
 	public String getToken(){
 		return this.token;
+	}
+	public String getLexema(){
+		return this.lexema;
 	}
 }
