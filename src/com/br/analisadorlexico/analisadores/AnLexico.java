3d5a77f86543b279b0da2b8e0f1fa96ec326490f
Julia Benatti <juliabenattilatanzio@gmail.com>
2014-09-13 14:56:29 -0300

    Modificações - 13/09 (14:56)

--- src/com/br/analisadorlexico/analisadores/AnLexico.java
+++ src/com/br/analisadorlexico/analisadores/AnLexico.java
@@ -1,74 +1,190 @@
 package com.br.analisadorlexico.analisadores;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.br.analisadorlexico.componentes.TabSimbolos;
 import com.br.analisadorlexico.componentes.Token;
+import com.br.analisadorlexico.leitorarquivo.LeitorDeArquivo;
 
 public class AnLexico {
-	private List<String> palavrasEncontradas = new ArrayList<String>();
-	private List<Token> tokensEncontrados = new ArrayList<Token>();
-	private List<String> errosEncontrados = new ArrayList<String>();
-	private TabSimbolos tabSimbolos = new TabSimbolos();
-	private ErrorHandler erroHandler = new ErrorHandler();
-	private String palavraAtual = "";
+	private String caminhoArquivo;
+	private String lexema;
+	private char caracter;
+	private LeitorDeArquivo leitorArquivo;
 
-	public void lerPalavra(char simbolo) {
-		// checar se é um espaço em branco
-		if (" ".equals(simbolo)) {
-			System.out
-					.print("Terminador espaço em branco encontrado. Analisar próximo token");
+	// Cria instância da classe do professor
 
-			// Checar se a palavra atual não é vazia
-			if (!palavraAtual.isEmpty()) {
-				palavrasEncontradas.add(palavraAtual);
-				palavraAtual = "";
-			}
-		}
+	// private List<String> palavrasEncontradas = new ArrayList<String>();
+	// private List<Token> tokensEncontrados = new ArrayList<Token>();
+	// private List<String> errosEncontrados = new ArrayList<String>();
+	// private TabSimbolos tabSimbolos = new TabSimbolos();
+	// private ErrorHandler erroHandler = new ErrorHandler();
+	// private String palavraAtual = "";
+	//
+	// public void lerPalavra(char simbolo) {
+	// // checar se é um espaço em branco
+	// if (" ".equals(simbolo)) {
+	// System.out
+	// .print("Terminador espaço em branco encontrado. Analisar próximo token");
+	//
+	// // Checar se a palavra atual não é vazia
+	// if (!palavraAtual.isEmpty()) {
+	// palavrasEncontradas.add(palavraAtual);
+	// palavraAtual = "";
+	// }
+	// }
+	//
+	// else if (";".equals(simbolo)) {
+	// // Checar se a palavra atual não é vazia
+	// if (!palavraAtual.isEmpty()) {
+	// palavrasEncontradas.add(palavraAtual);
+	// palavraAtual = "";
+	// }
+	// palavrasEncontradas.add(";");
+	// }
+	//
+	// else {
+	// palavraAtual += simbolo;
+	// }
+	//
+	// }
+	//
+	// public List<String> getListaPalavras() {
+	// return palavrasEncontradas;
+	// }
+	//
+	// public boolean encontreToken() {
+	// boolean codigoValido = true;
+	// Token token = new Token();
+	//
+	// for (String simbolo : palavrasEncontradas) {
+	// token = tabSimbolos.pesquisaPalavra(simbolo);
+	//
+	// // Verifica se um token foi encontrado
+	// if (token != null)
+	// tokensEncontrados.add(token);
+	//
+	// // Caso não ache o erro, cadastra novo erro
+	// else
+	// errosEncontrados.add("Palavra " + simbolo
+	// + " é inválida nesse formato e na linguagem."
+	// + "Coluna: Linha: ");
+	// }
+	//
+	// if (errosEncontrados.size() != 0) {
+	// codigoValido = false;
+	// erroHandler.setError(errosEncontrados);
+	//
+	// }
+	//
+	// return codigoValido;
 
-		else if (";".equals(simbolo)) {
-			// Checar se a palavra atual não é vazia
-			if (!palavraAtual.isEmpty()) {
-				palavrasEncontradas.add(palavraAtual);
-				palavraAtual = "";
-			}
-			palavrasEncontradas.add(";");
-		}
-
-		else {
-			palavraAtual += simbolo;
-		}
+	public AnLexico(String caminhoArquivo) {
+		this.caminhoArquivo = caminhoArquivo;
 
 	}
 
-	public List<String> getListaPalavras() {
-		return palavrasEncontradas;
+	public Token nextToken(){
+		
+		while (true){
+			//Chama, da classe do professor, a leitura do arquivo, passando como parâmetro o caminho
+			caracter = leitorArquivo.retornaChar(caminhoArquivo); //modificar para a classe real.
+			
+			switch(caracter){
+			
+			//Verifique se o caracter (que é único, é válido) 
+				case '+':
+				case '-':
+				case '*':
+				case '/':
+				case ';':
+				case '(':
+					return pesquisaToken(String.valueOf(caracter));
+					
+				default:
+					// Verifica se é espaço em branco
+					if (Character.isWhitespace(caracter)){
+						continueLeitura(' ');
+						continue;
+					}
+					// Verifica se é comentário
+					if (caracter == '#'){
+						continueLeitura('#');
+						continue;
+					}
+					// Verifica se é um número
+					if (Character.isDigit(caracter)){
+						int indicePonto = 0,indiceE  = 0;
+						boolean valido = true;
+						
+						while (true){
+							lexema += String.valueOf(caracter);
+							caracter = leitorArquivo.retornaChar(caminhoArquivo);
+							if(caracter == '.'  && indicePonto == 0 ){
+								lexema += String.valueOf(caracter);
+								indicePonto++;
+							}
+							if(caracter == 'E' && indiceE == 0 ){
+								lexema += String.valueOf(caracter);
+								indiceE++;
+							}
+							if(caracter == '.'  && indicePonto == 1 ){
+								//Mando erro para Error Handdler
+							}
+							if(caracter == 'E' && indiceE == 1 ){
+								//Mando erro para Error Handdler
+							}
+						}
+					}
+					// Verifica se é uma letra
+					if (Character.isLetter(caracter)){
+						
+					}
+					//Verifica se é um "_"
+					if (caracter == '_'){
+						
+					}
+			
+			}
+		}
+	}
+	// Descartar espaços em branco
+	// Descarta comentários
+	// switch (c){
+	//
+	// case '(':
+	// faça algo
+	// case '"':
+	// faça algo
+	// default:
+	// if (Character.isLetter(c) ou _){
+	// while (Character.isLetter(c) ou número ou _){
+	// lexema += c;
+	// c = Chama, da classe do professor, a leitura do arquivo, passando como
+	// parâmetro o caminho novamente (se necessário)
+	//
+	// }
+	// }
+	// Token de retorno = chama o método reconhecerId(lexema)
+	// retorno token;
+	// caso seja null, tratar
+	// no caso de letras, inserir na tabela de simbolos, caso seja válido, com
+	// linha e coluna (oferecidos pelo professor)
+	// Senão, salvar erro
+
+	public Token pesquisaToken(String palavra) {
+		return null;
 	}
 
-	public boolean encontreToken() {
-		boolean codigoValido = true;
-		Token token = new Token();
+	public void erroEncontrado() {
 
-		for (String simbolo : palavrasEncontradas) {
-			token = tabSimbolos.pesquisaPalavra(simbolo);
+	}
 
-			// Verifica se um token foi encontrado
-			if (token != null)
-				tokensEncontrados.add(token);
-
-			// Caso não ache o erro, cadastra novo erro
-			else
-				errosEncontrados.add("Palavra " + simbolo
-						+ " é inválida nesse formato e na linguagem."
-						+ "Coluna: Linha: ");
+	public void continueLeitura(char c) {
+		// enquanto não encontrar o fim do comentário
+		while (true) {
+			if (caracter == c) {
+				break;
+			}
+			caracter = leitorArquivo.retornaChar(caminhoArquivo);
 		}
-
-		if (errosEncontrados.size() != 0) {
-			codigoValido = false;
-			erroHandler.setError(errosEncontrados);
-
-		}
-
-		return codigoValido;
 	}
 }
+
--- src/com/br/analisadorlexico/analisadores/ErrorHandler.java
+++ src/com/br/analisadorlexico/analisadores/ErrorHandler.java
@@ -1,10 +1,15 @@
 package com.br.analisadorlexico.analisadores;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ErrorHandler {
+	List <String> erros = new ArrayList<String>();
 	public void setError(List<String> erros){
 		for (String erro : erros)
 		System.out.println(erro);
+	}
+	public List <String> getErrors(){
+		return erros;
 	}
 }
