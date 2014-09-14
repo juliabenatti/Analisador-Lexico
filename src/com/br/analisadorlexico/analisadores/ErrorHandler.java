d992fe217e0e35cb3f395bb9786569c9d7555a2c
Julia Benatti <juliabenattilatanzio@gmail.com>
2014-09-13 21:39:54 -0300

    Error Handler - código comentado, verificar erros

--- src/com/br/analisadorlexico/analisadores/ErrorHandler.java
+++ src/com/br/analisadorlexico/analisadores/ErrorHandler.java
@@ -1,27 +1,27 @@
-package com.br.analisadorlexico.analisadores;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class ErrorHandler {
-
-	try {  
-	     File file = new File("filename");  
-	   
-	     // Create file if it does not exist  
-	     boolean success = file.createNewFile();  
-	     if (success) {  
-	         // File did not exist and was created  
-	     } else {  
-	         // File already exists  
-	     }  
-	     BufferedWriter out = new BufferedWriter(new FileWriter("outfilename"));  
-	        out.write("aString");  
-	        out.close();
-	 } catch (IOException e) {  
-	 }
-}
-
+//package com.br.analisadorlexico.analisadores;
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ErrorHandler {
+//
+//	try {  
+//	     File file = new File("filename");  
+//	   
+//	     // Create file if it does not exist  
+//	     boolean success = file.createNewFile();  
+//	     if (success) {  
+//	         // File did not exist and was created  
+//	     } else {  
+//	         // File already exists  
+//	     }  
+//	     BufferedWriter out = new BufferedWriter(new FileWriter("outfilename"));  
+//	        out.write("aString");  
+//	        out.close();
+//	 } catch (IOException e) {  
+//	 }
+//}
+//
