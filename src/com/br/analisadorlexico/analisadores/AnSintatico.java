ad073111a7821d7065cb5cdefb80f1b28214b4a8
Julia Benatti <juliabenattilatanzio@gmail.com>
2014-09-14 17:23:10 -0300

    Merge entre commits

--- src/com/br/analisadorlexico/analisadores/AnSintatico.java
+++ src/com/br/analisadorlexico/analisadores/AnSintatico.java
@@ -1,21 +1,13 @@
 package com.br.analisadorlexico.analisadores;
 
-import javax.swing.JOptionPane;
-
-import com.br.analisadorlexico.componentes.Token;
-
 public class AnSintatico {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		String caminho = JOptionPane.showInputDialog("Digite o caminho do arquivo");
-		AnLexico analisadorLexico = new AnLexico(caminho);
-		Token tk = new Token();
-		
-		do{
-			tk = analisadorLexico.nextToken();
-			//System.out.println("Token: "+tk.getNomeToken()+" Lexema: "+tk.getValor());
-			
-		}while (false);
+		// TODO Auto-generated method stub
+
 	}
 
 }
