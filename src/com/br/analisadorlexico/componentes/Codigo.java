54eab7268878de9d9cead1f1b74ae687b7667e53
root <root@localhost>
2014-09-18 18:17:24 -0300

    teste 666 hu3 hu3 brbr

--- src/com/br/analisadorlexico/componentes/Codigo.java
+++ src/com/br/analisadorlexico/componentes/Codigo.java
@@ -19,7 +19,7 @@ public class Codigo {
 	public String getToken(){
 		return this.token;
 	}
-	public String getLexema(){ // teste 666
+	public String getLexema(){ // teste 666 hu3 hu3 brbr
 		return this.lexema;
 	}
 }
