90f1cb443997cecb08a1d25e0028a768c62d1d9e
TheEld <dantemm13@gmail.com>
2014-10-01 23:09:22 -0300

    Implementação do ErrorHandler e criacao da global caminhoArquivo na AnLexico

--- src/com/br/analisadorlexico/analisadores/ErrorHandler.java
+++ src/com/br/analisadorlexico/analisadores/ErrorHandler.java
@@ -1,5 +1,7 @@
 package com.br.analisadorlexico.analisadores;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +12,26 @@ public class ErrorHandler {
 	private List<String> erros = new ArrayList();
 	
 	public void setError(String erro){
-		System.out.println(erro);
 		erros.add(erro);
 	}
+	
+	public void writeError(){
+		
+		try{
+			File file = new File(AnLexico.caminhoArquivo, "banco.txt");
+			FileWriter arquivo = new FileWriter(file);
+			for(String e : erros){
+			arquivo.write(e);
+			}
+		arquivo.close();
+		}catch(Exception e){
+			System.out.println("Arquivo não encontrado.");
+		}
+		
+	}
+	
 	public static ErrorHandler getInstance() {
 		return instancia;
-	}
-	public List<String> getErrors(){
-		return erros;
 	}
 	
 }
